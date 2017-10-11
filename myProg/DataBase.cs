486eedbd522d7ee4cf294fbd3e1774ad23ec0fb5
Kritzinger Simon <13krisim@TFO.local>
2017-10-11 08:31:23 +0200

    First Commit

---
+++ myProg/DataBase.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 13krisim
+ * Date: 11.10.2017
+ * Time: 08:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace myProg
+{
+	/// <summary>
+	/// Description of DataBase.
+	/// </summary>
+	public class DataBase
+	{
+		public DataBase()
+		{
+		}
+	}
+}
---
+++ myProg/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 13krisim
+ * Date: 11.10.2017
+ * Time: 08:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace myProg
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
